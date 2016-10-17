54196e6ca2b392ef93a45afc2a10b46b8ea5ad26
Daniel Su <email@danielsu.com>
2016-10-17 11:12:01 -0400

    feat: initial working script

---
+++ README.md
@@ -0,0 +1,27 @@
+## aws-s3-lambda-mongo-backup
+Lambda script for backup mongo DB and pipe to S3.
+
+### Dependencies
+/bin mongodump binary from mongodb.com. 64bit Amazon AMI
+
+```bash
+$ npm i moment fs path util
+```
+### Configure
+
+```bash
+$ cp config.json.sample config.json
+```
+
+Sample config.json
+
+```
+json
+```
+
+
+### Deploy
+
+### Notes
+
+
---
+++ index.js
@@ -0,0 +1,48 @@
+// dependencies
+var AWS = require('aws-sdk'); //Provided by lambda (no need to install)
+var fs   = require("fs");
+var path = require("path");
+var util = require('util');
+var moment = require('moment');
+
+// get reference to S3 client
+var s3 = new AWS.S3();
+
+exports.handler = function(event, context) {
+  // Load config.json
+  var configPath = path.resolve(__dirname, "config.json");
+  var config = JSON.parse(fs.readFileSync(configPath, { encoding: "utf8" }));
+
+  // Read options from the event.
+  console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+  var dstBucket = config.dstBucket; //from config.json
+  var folder = moment().format('YYYY-MM-DD HH.mm.ss');
+  var dstKey = 'databaseDump.mongo';
+
+  process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+
+  var exec = require('child_process').exec;
+  var cmd = './bin/mongodump --archive --db '.config.mongo.db.' --username '.config.mongo.username.' --password '.config.mongo.password.' --host '.config.mongo.host.'';
+  console.log(cmd);
+
+  exec(cmd, function(error, stdout, stderr) {
+    if (error){
+      console.log(error);
+    } else {
+      s3.putObject(
+        {
+          Bucket: dstBucket,
+          Key: folder + '/' + dstKey,
+          Body: stdout
+        },
+        function(err, data) {
+          if (err){
+            console.log(err);
+          } else {
+            console.log("Successfully uploaded data");
+          }
+        }
+      );
+    }
+  });
+};
